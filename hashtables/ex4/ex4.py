de2b9dc09773e4d73ff887970119d64f72b88a3b
Nicole white <you@example.com>
2020-06-04 13:02:34 -1000

    finished 4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,15 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    # dict to hold all nums in a
+    storage = {}
+    # arr to return matches in absolute value(result)
+    result = []
+    for num in a:
+        # if the num has a corresponding number in dict
+        if storage.get(abs(num)): # .get method returns the value for the given key
+            result.append(abs(num)) # if so add to results list
+        else:
+            # if no val is found pass num a new key along with its value
+            storage[abs(num)] = num
 
     return result
 
